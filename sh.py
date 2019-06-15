7f2456a2f8eb4bdcfb8521040390fd6529cca05c
rilfv <51865391+rilfv@users.noreply.github.com>
2019-06-16 04:58:32 +0700

    new

---
+++ sh.py
@@ -0,0 +1,45 @@
+figlet Mr.Ril Tools | lolcat
+
+echo
+
+echo "_____________________________________________________"
+echo " Created by: BerrilFv               $white                     " | lolcat
+echo " Contact me: 085711990416 (sekalian pulsa ea:v)       $white  " | lolcat
+echo " ____________________________________________________"
+
+echo "loading ..." | lolcat
+sleep 1
+echo $red "5%" | lolcat
+sleep 2
+echo $red "10%" | lolcat
+sleep 3
+echo $red "30%" | lolcat
+sleep 4
+echo $red "50%" | lolcat
+sleep 5
+echo $red "70%" | lolcat
+sleep 5
+echo $red "95%" | lolcat
+sleep 6
+echo $red "100%" | lolcat
+sleep 7
+echo "finish" |lolcat
+echo
+echo
+echo "============================================" | lolcat
+echo "1. Sadap Whats App (No Root)                " | lolcat
+echo "============================================" | lolcat
+echo "2. Hack Instagram Target                    " | lolcat
+echo "============================================" | lolcat
+echo " MONGGO DI PILIH HEHE " | lolcat 
+echo "╭─Mr.Ril" |lolcat
+read -p "╰─#" pil;
+
+
+case $pil in
+1) pkg install nmap
+echo "${y} {1} Masukkan Web${endc}:"
+read web
+nmap $web
+echo
+
