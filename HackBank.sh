5aac24299b48ef62a80cd78942655ff2af28bb56
rilfv <51865391+rilfv@users.noreply.github.com>
2019-07-07 22:27:52 +0700

    New upload

---
+++ HackBank.sh
@@ -0,0 +1,25 @@
+echo "#####################################
+# Created by:Mr.B-CT                #   
+# Contac Me :Bct5087@gmail.com      #
+#####################################" | lolcat
+sleep 1
+echo "GUNAKAN TOOLS INI DENGAN BIJAK" | lolcat
+echo "SEGALA RESIKO BUKAN TANGGUNG JAWAB KAMI" | lolcat
+sleep 2
+
+
+
+echo "Masukan no rek Anda :" | figlet
+read nomor rekening
+echo "Masukan no rek Target :" | figlet
+read nomor rekening
+echo "Mohon tunggu proses sedang berjalan.." | figlet
+sleep 3
+echo " Memuat.." | figlet
+sleep 2
+echo "PROSES SELSAI" | figlet
+sleep 2 
+figlet GUNAKAN TOOLS INI DENGAN BIJAK | lolcat
+sleep 1
+clear
+
