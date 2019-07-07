f8113e361367c6d6f9d8851c1805c21e370f56c3
rilfv <51865391+rilfv@users.noreply.github.com>
2019-07-07 22:34:51 +0700

    Update HackBank.sh

--- HackBank.sh
+++ HackBank.sh
@@ -2,24 +2,24 @@ echo "#####################################
 # Created by:Mr.B-CT                #   
 # Contac Me :Bct5087@gmail.com      #
 #####################################" | lolcat
-sleep 1
-echo "GUNAKAN TOOLS INI DENGAN BIJAK" | lolcat
-echo "SEGALA RESIKO BUKAN TANGGUNG JAWAB KAMI" | lolcat
 sleep 2
-
-
-
-echo "Masukan no rek Anda :" | lolcat
-read nomor rekening
-echo "Masukan no rek Target :" | lolcat
-read nomor rekening
-echo "Mohon tunggu proses sedang berjalan.." | lolcat
+echo #GUNAKAN TOOLS INI DENGAN BIJAK 
+echo #SEGALA RESIKO BUKAN TANGGUNG JAWAB KAMI
 sleep 3
-echo " Memuat.." | lolcat
+
+
+
+echo "Masukan no rek Anda :"
+read nomor rekening
+echo "Masukan no rek Target :" 
+read nomor rekening
+echo "Mohon tunggu proses sedang berjalan.."
+sleep 3
+echo " Memuat.." 
 sleep 2
-echo "PROSES SELSAI" | lolcat
+echo "PROSES SELSAI"
 sleep 2 
-figlet GUNAKAN TOOLS INI DENGAN BIJAK | lolcat
-sleep 1
+echo #GUNAKAN TOOLS INI DENGAN BIJAK 
+sleep 4
 clear
 
